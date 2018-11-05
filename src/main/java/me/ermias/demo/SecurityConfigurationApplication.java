aca709db489e8034ef498d1a42f77949a917b373
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-11-05 16:27:02 -0500

    Security

---
+++ src/main/java/me/ermias/demo/SecurityConfigurationApplication.java
@@ -0,0 +1,12 @@
+package me.ermias.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityConfigurationApplication.class, args);
+	}
+}
---
+++ src/main/java/me/ermias/demo/UserRepository.java
@@ -0,0 +1,7 @@
+package me.ermias.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
